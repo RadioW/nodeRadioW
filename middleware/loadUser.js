41a8c55c75ef4a2648f87b417d732b869fa416ca
Arhangel64 <Arhangel64@bk.ru>
2014-12-22 23:59:49 +0300

    lil refactor. Now sessions lings for two years, user loads from base only for routes

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -1,7 +1,6 @@
 var User = require('../models/user').User;
 
 module.exports = function(req, res, next) {
-	req.self = res.locals.self = null;
 	req.serverInfo = res.locals.serverInfo = req.app.get('serverInfo');
 	if (!req.session.user) return next();
 
